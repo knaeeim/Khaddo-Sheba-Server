710e82f747b2e37f9da0c26a5896ca19529497aa
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2025-06-04 15:32:02 +0530

    get all food make dynamic

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const cors = require("cors");
 const admin = require("firebase-admin");
 const port = process.env.PORT || 3000;
 // Firebase admin service account key decoded from base64
-const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString("utf8");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+    "utf8"
+);
 // convert into the JSON
 const serviceAccount = JSON.parse(decoded);
-
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -19,27 +20,24 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 
-const verifyFirebaseToken = async (req, res, next ) => {
-    const authHeader = req.headers?.authorization; 
-    if(!authHeader || !authHeader.startsWith("Bearer ")){
+const verifyFirebaseToken = async (req, res, next) => {
+    const authHeader = req.headers?.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
         return res.status(401).send({ message: "Unauthorized access" });
     }
 
     const token = authHeader.split(" ")[1];
-    
+
     try {
         const decodedToken = await admin.auth().verifyIdToken(token);
         req.decodedToken = decodedToken;
         next();
-    }
-    catch (error) {
+    } catch (error) {
         return req.status(401).send({ message: "Unauthorized access" });
     }
-} 
+};
 
-
-
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plgxbak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.plgxbak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -55,54 +53,74 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
-        // Database and collection will be created here 
+        // Database and collection will be created here
         const foodCollection = client.db("foodDB").collection("foods");
-        const requestedFoodsCollection = client.db("foodDB").collection("requestedFoods");
+        const requestedFoodsCollection = client
+            .db("foodDB")
+            .collection("requestedFoods");
 
-
-        // api starts 
-        // testing api 
+        // api starts
+        // testing api
         app.get("/", (req, res) => {
             res.send("Server is running");
-        })
+        });
 
         // get all foods
         app.get("/foods", async (req, res) => {
-            const foods = await foodCollection.find().sort({date: 1}).toArray();
-            res.send(foods);
-        })
+            const query = req.query.sortBy;
+            const limit = parseInt(req.query.limit) || 0;
 
-        // get single food by id 
-        app.get("/foods/:id",  verifyFirebaseToken, async (req, res) => {
+            let sortQuery = {};
+
+            if (query === "date") {
+                sortQuery = { date: 1 };
+            } else if (query === "quantity") {
+                sortQuery = { foodQuantity: -1 };
+            }
+
+            const today = new Date(); 
+            today.setHours(0, 0, 0, 0); // Set time to midnight to compare only dates
+            const foods = await foodCollection
+                .find({ date : { $gte: today }})
+                .sort(sortQuery).limit(limit)
+                .toArray();
+
+            res.send(foods);
+        });
+
+        // get single food by id
+        app.get("/foods/:id", verifyFirebaseToken, async (req, res) => {
             const id = req.params.id;
-            const query = { _id : new ObjectId(id) };
+            const query = { _id: new ObjectId(id) };
             const food = await foodCollection.findOne(query);
             res.send(food);
-        })
+        });
 
         app.post("/addFood", verifyFirebaseToken, async (req, res) => {
             const food = req.body;
 
             const decodedTokenEmail = req.decodedToken.email;
             console.log(decodedTokenEmail);
-            
-            if(req.body.email !== decodedTokenEmail){
+
+            if (req.body.email !== decodedTokenEmail) {
                 return res.status(403).send({ message: "Forbidden access" });
+            }
+
+            if(food.date) {
+                food.date = new Date(food.date);
             }
 
             console.log(food);
             const result = await foodCollection.insertOne(food);
             res.send(result);
-        })
+        });
 
         app.delete("/foods/:id", async (req, res) => {
-            const id = req.params.id; 
+            const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await foodCollection.deleteOne(query);
-            res.send(result)
-        })
-        
-
+            res.send(result);
+        });
 
         // requested foods API's
         app.get("/myRequestedFoods", verifyFirebaseToken, async (req, res) => {
@@ -110,27 +128,26 @@ async function run() {
             console.log(email);
             const decodedTokenEmail = req.decodedToken.email;
 
-            if(email !== decodedTokenEmail ){
-                return res.status(403).send({ message: "Forbidden Access"})
+            if (email !== decodedTokenEmail) {
+                return res.status(403).send({ message: "Forbidden Access" });
             }
 
             // now find the foods by email
-            const query = { requestedUserEmail: email }
-            const foods = await requestedFoodsCollection.find(query).toArray(); 
+            const query = { requestedUserEmail: email };
+            const foods = await requestedFoodsCollection.find(query).toArray();
             res.send(foods);
-        })
+        });
 
         app.post("/myRequestedFoods", verifyFirebaseToken, async (req, res) => {
             const food = req.body;
             console.log(food);
-            const decodedTokenEmail = req.decodedToken.email
-            if(food.requestedUserEmail !== decodedTokenEmail){
+            const decodedTokenEmail = req.decodedToken.email;
+            if (food.requestedUserEmail !== decodedTokenEmail) {
                 return res.status(403).send({ message: "Forbidden access" });
             }
             const result = await requestedFoodsCollection.insertOne(food);
             res.send(result);
-        })
-
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
